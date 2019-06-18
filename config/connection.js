8752dbb22499b398e37848e18c071a4c293ef649
Adventure 2019 <caliew26@gmail.com>
2019-06-18 14:13:15 -0400

    changed the config again

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,14 @@ var mysql = require("mysql");
 
 //define msql connection values
 var connection;
-if (process.env.JAWSDB_URL) {
-      connection = mysql.createConnection(process.env.JAWSDB_URL);
+if ((process.env.PORT || 8080) !=8080) {
+      connection = mysql.createConnection({
+          host: "jlg7sfncbhyvga14.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+          port: 3306,
+          user: "n9ct6pjqxgzwea7d",
+          password: "n9ct6pjqxgzwea7d",
+          database: "wqnbw4oazpnu7388"
+      });
   } else {
       connection = mysql.createConnection({
         host: "localhost",
