39cb710f1c0732986cf4fd6e87948e611dbd8e8c
Adventure 2019 <caliew26@gmail.com>
2019-06-18 14:07:16 -0400

    working on the jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -2,21 +2,16 @@
 var mysql = require("mysql");
 
 //define msql connection values
-if ((process.env.PORT || 3306) != 3306) {
-      var connection = mysql.connection({
-        host: "jlg7sfncbhyvga14.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-        port: 3306,
-        user: "n9ct6pjqxgzwea7d",
-        password: "bl0pcoc23ojg1n6m",
-        database: "wqnbw4oazpnu7388"
-      })
+var connection;
+if (process.env.JAWSDB_URL) {
+      connection = mysql.createConnection(process.env.JAWSDB_URL);
   } else {
-      var connection = mysql.createConnection({
-          host: "localhost",
-          port: 3306,
-          user: "root",
-          password: "Potter!!13",
-          database: "burgers_db"
+      connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "Potter!!13",
+        database: "burgers_db"
       })
 }
 
