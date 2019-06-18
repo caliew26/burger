e9a8e6e8e2ba4a41609e853f0b0bebac844d26bc
Adventure 2019 <caliew26@gmail.com>
2019-06-18 13:22:42 -0400

    added a new connection for heroku that is an ifelse statement - got this from Ryan in slack

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,31 @@
 var mysql = require("mysql");
 
 //define msql connection values
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Potter!!13",
-  database: "burgers_db"
-});
+var con = {}
+  if ((process.env.PORT || 3000) != 3000) {
+      con = {
+        host: "gmgcjwawatv599gq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        port: 3306,
+        user: "yqr1358ag59b86t2",
+        password: "",
+        database: "mfprp122v8lffehd"
+      }
+  } else {
+      con = {
+          host: "localhost",
+          port: 3306,
+          user: "root",
+          password: "",
+          database: "burgers_db"
+      }
+}
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "Potter!!13",
+//   database: "burgers_db"
+// });
 
 //establish connection
 connection.connect(function(err) {
