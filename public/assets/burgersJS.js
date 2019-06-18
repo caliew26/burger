7782c65e4fcdc09b7e63c7e3f0c7c9f5f8c0272d
Adventure 2019 <caliew26@gmail.com>
2019-06-18 18:02:54 -0400

    added config pssword again

--- public/assets/burgersJS.js
+++ public/assets/burgersJS.js
@@ -1,14 +1,17 @@
 //this is where the js will live - listening for the onclick of the button on the index page
 
 $(function(){
-    $(".create-form").click(function(event) {
+    $(".newBurgerBtn").on("click", function(event) {
+        console.log("This is the onclick");
         //don't want the page to auto-reload
         event.preventDefault();
 
         var newBurger = {
             burger_name: $("#burgerY").val().trim(),
             devoured: $("[name=devoured]:checked").val().trim()
-        };
+        }
+            console.log(newBurger.burger_name);
+            console.log(newBurger.devoured);
         $.ajax("/api/burgers", {
             type: "POST",
             data: newBurger
